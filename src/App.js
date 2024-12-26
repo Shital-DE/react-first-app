c3cd3878495f36bfb274c14321f15c812f07c5f3
Shital-DE <dattaentind@gmail.com>
2024-12-26 10:31:29 +0530

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <>
       <Navbar title="Text Utils" aboutText="About us" />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
+        {/* <About/> */}
       </div>
       
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,24 +4,53 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("Uppercase was clicked");
-        // let newText = text.toUpperCase();
-        // setText(newText);
+        setText(text.toUpperCase());
     }
 
-     const handleOnChanged = ()=>{
-        console.log("On changed");
-        // let newText = text.toUpperCase();
-        // setText(newText);
+    const handleLwClick = ()=>{
+        setText(text.toLowerCase());
     }
-    const [text, setText] = useState('Enter text here');
+
+    const handleClearClick = ()=>{
+        setText('');
+    }
+
+    const handleOnChange = (event)=>{
+         setText(event.target.value);
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const [text, setText] = useState('');
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChanged} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLwClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
         </div>
+        <div className="container my-3">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length}  words, {text.length} characters and {text.split(/[.!?]/).filter(sentence => sentence.trim() !== '').length} sentences.</p>
+                <p>{0.008 * text.split(" ").length} minutes read.</p>
+                <h2>Preview</h2>
+                <p>{ text}</p>
+        </div>
+            </>
     )
 }
