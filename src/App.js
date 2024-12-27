0d30ea5850c1e3a555c1232d0a3eef0f47346cb7
Shital-DE <dattaentind@gmail.com>
2024-12-27 17:24:19 +0530

    Removed gh pages  part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -44,21 +44,21 @@ function App() {
   }  
   return (
     <>
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
       <Navbar title="Text Utils"
         mode={mode} toggleMode={toggleMode}
         aboutText="About"
       />
       <Alert alert={ alert} />
       <div className="container my-3">
-        {/* <Routes> */}
-            {/* <Route exact path="/" element={ */}
+        <Routes>
+            <Route exact path="/" element={
               <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-            {/* } /> */}
-          {/* <Route exact path="/about" element={<About/>} /> */}
-        {/* </Routes> */}
+             } /> 
+          <Route exact path="/about" element={<About/>} />
+        </Routes>
       </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </>
   );
 }
